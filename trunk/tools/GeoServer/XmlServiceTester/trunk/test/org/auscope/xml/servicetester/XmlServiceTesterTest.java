59bd215a893540158c82a5817f1d33de3844ad33
car605 <car605@910a3654-de2c-0410-bd95-d62e98ccebcd>
2008-05-26 07:17:42 +0000

    All integration tests working.
    git-svn-id: https://svnserv.csiro.au/svn/ext/GeoSciML@2363 910a3654-de2c-0410-bd95-d62e98ccebcd

--- trunk/tools/GeoServer/XmlServiceTester/trunk/test/org/auscope/xml/servicetester/XmlServiceTesterTest.java
+++ trunk/tools/GeoServer/XmlServiceTester/trunk/test/org/auscope/xml/servicetester/XmlServiceTesterTest.java
@@ -9,8 +9,40 @@ import junit.framework.TestCase;
 public class XmlServiceTesterTest extends TestCase {
 
     public static void test() {
-        assertTrue(XmlServiceTester.run(new File(
-                "testresource/test-suites/test-suite.XmlServiceTesterTest.xml")));
+        assertTrue(XmlServiceTester
+                .run(new File(
+                        "testresource/test-suites/test-suite.XmlServiceTesterTest.xml")));
+    }
+
+    /**
+     * Test with schema-valid ExceptionReport.
+     */
+    public static void testWrongContent() {
+        assertFalse(XmlServiceTester
+                .run(new File(
+                        "testresource/test-suites/test-suite.XmlServiceTesterTest.wrong-content.xml")));
+    }
+
+    /**
+     * Test with schema-invalid content.
+     */
+    public static void testBroken() {
+        assertFalse(XmlServiceTester
+                .run(new File(
+                        "testresource/test-suites/test-suite.XmlServiceTesterTest.broken.xml")));
+    }
+
+    /**
+     * Test with test-suite that does not exist. A test that ensures
+     * XmlServiceTester.run() throws an exception.
+     */
+    public static void testTestSuiteDoesNotExist() {
+        try {
+            XmlServiceTester.run(new File("test-suite.does-not-exist.xml"));
+            fail();
+        } catch (RuntimeException e) {
+            // success
+        }
     }
 
 }
