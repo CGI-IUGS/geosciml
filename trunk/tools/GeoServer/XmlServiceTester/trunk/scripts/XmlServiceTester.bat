48d5bac885a6d58070cfd56eedee03bd09d9bab2
car605 <car605@910a3654-de2c-0410-bd95-d62e98ccebcd>
2008-06-09 07:36:34 +0000

    Fixed line endings and added endorsed dirs for JAXB overrides in scripts. Updated documentation to remove raw runnable JAR example.
    git-svn-id: https://svnserv.csiro.au/svn/ext/GeoSciML@2394 910a3654-de2c-0410-bd95-d62e98ccebcd

--- trunk/tools/GeoServer/XmlServiceTester/trunk/scripts/XmlServiceTester.bat
+++ trunk/tools/GeoServer/XmlServiceTester/trunk/scripts/XmlServiceTester.bat
@@ -1,1 +1,2 @@
-@java -Xmx512m -jar "%~dp0\..\lib\XmlserviceTester.jar" %*
+REM endorsed dirs mechanism required to override JAXB in boot classpath
+@java -Xmx512m -Djava.endorsed.dirs="%~dp0\..\lib" -jar "%~dp0\..\lib\XmlserviceTester.jar" %*
--- trunk/tools/GeoServer/XmlServiceTester/trunk/scripts/XmlServiceTester.sh
+++ trunk/tools/GeoServer/XmlServiceTester/trunk/scripts/XmlServiceTester.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec java -Xmx512m -jar "`dirname $0`/../lib/XmlServiceTester.jar" "$*"
+BASEDIR="`dirname $0`/.."
+# endorsed dirs mechanism required to override JAXB in boot classpath
+exec java -Xmx512m -Djava.endorsed.dirs="$BASEDIR/lib" \
+    -jar "$BASEDIR/lib/XmlServiceTester.jar" "$*"
