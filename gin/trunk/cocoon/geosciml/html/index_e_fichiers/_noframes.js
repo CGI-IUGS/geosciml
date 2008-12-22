62ae5a440a16da4b3f15d3911db9154199af5c8a
mase <mase@910a3654-de2c-0410-bd95-d62e98ccebcd>
2008-12-22 12:30:49 +0000

    Deleted GSC Testbed 3 specific static pages.
    git-svn-id: https://svnserv.csiro.au/svn/ext/GeoSciML@2747 910a3654-de2c-0410-bd95-d62e98ccebcd

--- gin/trunk/cocoon/geosciml/html/index_e_fichiers/_noframes.js
+++
@@ -1,7 +0,0 @@
-if(self.parent.frames.length!=0){
-  if(self.parent.location.replace){
-    self.parent.location.replace(document.location);
-  }else{
-    self.parent.location=document.location;
-  }
-}
