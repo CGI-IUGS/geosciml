9ee9a535011723871fbb5dcb358ea2b27c134d65
boisverte <boisverte@910a3654-de2c-0410-bd95-d62e98ccebcd>
2015-06-18 16:19:55 +0000

    deleted file need to be synched
    git-svn-id: https://svnserv.csiro.au/svn/ext/GeoSciML@4834 910a3654-de2c-0410-bd95-d62e98ccebcd

--- trunk/schemas/schemas_mandatory/generate_mandatory.bat
+++
@@ -1,7 +0,0 @@
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\geoSciMLBasic.xsd -xsl:nillable2mandatory.xslt -o:geoSciMLBasic.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\geoSciMLExtension.xsd -xsl:nillable2mandatory.xslt -o:geoSciMLExtension.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\borehole.xsd -xsl:nillable2mandatory.xslt -o:borehole.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\geologicSpecimen.xsd -xsl:nillable2mandatory.xslt -o:geologicSpecimen.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\laboratoryAnalysis.xsd -xsl:nillable2mandatory.xslt -o:laboratoryAnalysis.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\laboratoryAnalysis-Specimen.xsd -xsl:nillable2mandatory.xslt -o:laboratoryAnalysis-Specimen.xsd
-java -cp \saxon\saxon9he.jar net.sf.saxon.Transform -t -s:..\dating.xsd -xsl:nillable2mandatory.xslt -o:dating.xsd
