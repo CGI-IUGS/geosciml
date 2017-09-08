067c8e9a42aa253a49a553c64c82e289373bbc07
boisverte <boisverte@910a3654-de2c-0410-bd95-d62e98ccebcd>
2017-09-08 14:23:25 +0000

    first shot at JSON
    git-svn-id: https://svnserv.csiro.au/svn/ext/GeoSciML@5323 910a3654-de2c-0410-bd95-d62e98ccebcd

---
+++ trunk/ontology/gsml4_json.bat
@@ -0,0 +1,3 @@
+echo Starting OWL creation
+"C:\Program Files (x86)\Java\jre1.8.0_144\bin\java" -Djava.library.path=/sc/lib/ -jar /sc/ShapeChange-2.3.0.jar -Dfile.encoding=UTF-8 -c /sc/Ontology/gsml4js.xml
+
